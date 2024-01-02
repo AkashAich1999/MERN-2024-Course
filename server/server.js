375f4151a7106968cd970f07b27eda29113b19c0
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-01-02 10:39:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
 	res.status(200).send("Hello, I am Akash Aich");
 });
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+});
