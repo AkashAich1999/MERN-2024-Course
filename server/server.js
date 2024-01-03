8b9610a1d9159d25d0c33534646bdb9fca2cfdcd
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-01-03 10:46:28 +0530

    initial registration page route added

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
 	res.status(200).send("Hello, I am Akash Aich");
 });
 
+app.get("/register", (req, res) => {
+	res.status(200).send("Hello, Welcome to Registration Page");
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
