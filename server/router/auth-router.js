ea813c36300d7c6b4b5b1e5f0ad386f0245b5ac8
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-01-04 10:33:29 +0530

    Update auth-router.js

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // Mount the Router: To use the router in your main Express app, you can mount it at a specific URL prefix.
-router.get("/", (req, res) => {
-	res.status(200).send("Welcome, to Akash Technologies !");
+
+// One Way (1st Way)
+// router.get("/", (req, res) => {
+// 	res.status(200).send("Welcome, to Akash Technologies !");
+// });
+
+// Second Way
+router.route("/").get((req, res) => {
+    	res.status(200).send("Welcome, to Akash Technologies !");
 });
 
 module.exports = router;
