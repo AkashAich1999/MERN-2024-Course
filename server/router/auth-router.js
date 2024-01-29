169b13897409a56e4bae48747b4065a1fc873ee7
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-01-29 22:06:30 +0530

    Controllers in Express.js (Clean Code)

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { home, register } = require("../controllers/auth-controller");
-// const register = require("../controllers/auth-controller.js");
+// const { home, register } = require("../controllers/auth-controller");
+const authcontrollers = require("../controllers/auth-controller");
 
+router.route("/").get(authcontrollers.home);
 
-router.route("/").get(home);
-
-// router.route("/register").get((req, res) => {
-//     res.status(200).send("Welcome, to Registration Page !");
-// });
-
-router.route("/register").get(register);
+router.route("/register").get(authcontrollers.register);
 
 module.exports = router;
