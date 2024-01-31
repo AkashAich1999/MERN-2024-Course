4fc1d9c5a2029a3d5b8399bfadba256c9e277377
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-01-31 18:18:34 +0530

    Storing Registered User Data in MongoDB Database using Postman

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
     try{
         res.status(200).send("Welcome, to Akash Technologies !");
@@ -11,7 +13,17 @@ const register = async (req, res) => {
     try{
         console.log(req.body);
         // res.status(200).send("Welcome, to Registration Page !");
-        res.status(200).json({ message : req.body });
+        const {username, email, phone, password} = req.body;
+        const userExist = await User.findOne({email: email});
+
+        if(userExist){
+            return res.status(400).json({ msg : "Email Already Exists !"});
+        }
+
+        // If user does not Exists, then Create One
+        const userCreated = await User.create({ username, email, phone, password });
+
+        res.status(200).json({ userCreated });
     }
     catch(error){
         res.status(400).send({msg:"page not found"});
